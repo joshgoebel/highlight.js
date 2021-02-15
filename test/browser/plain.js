39f645bd771a3fecd01aa51ce0bb5c4877a50afe
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-02-15 15:39:48 +0100

    chore(test): refactor tests to use ESM syntax

--- test/browser/plain.js
+++ test/browser/plain.js
@@ -1,6 +1,4 @@
-'use strict';
-
-const {newTestCase, defaultCase, buildFakeDOM } = require('./test_case')
+import {newTestCase, defaultCase, buildFakeDOM } from './test_case.js';
 
 describe('plain browser', function() {
 
